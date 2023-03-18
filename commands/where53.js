9f62574d1f69139eb5dd3b7f4fff3d7486271256
PashaYakubovsky <bayntis@gmail.com>
2023-03-18 12:08:43 +0200

    new files

--- commands/where53.js
+++ commands/where53.js
@@ -1,17 +1,22 @@
 const { SlashCommandBuilder } = require("discord.js");
 const axios = require("axios");
+const { inboostToken } = require("../config.json");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("where53")
         .setDescription("I can tell you where 53 port"),
     async execute(interaction) {
-        const port = await axios.get("https://mhp.inboost.ai:5053/api/who", {
-            headers: {
-                Authorization: `bearer ${process.env.INBOOST_TOKEN}`,
-            },
-        });
+        try {
+            const port = await axios.get("https://mhp.inboost.ai:5053/api/who", {
+                headers: {
+                    Authorization: `bearer ${inboostToken}`,
+                },
+            });
 
-        await interaction.reply(port.data);
+            await interaction.reply(port.data);
+        } catch (err) {
+            console.log(err);
+        }
     },
 };
