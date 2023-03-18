70af9a167f3cdd782bf5d55e62f0646d2d9dd50d
Pashafd <bayntis@gmail.com>
2023-03-18 12:28:44 +0200

    update slash where

--- commands/where53.js
+++ commands/where53.js
@@ -7,16 +7,21 @@ module.exports = {
         .setName("where53")
         .setDescription("I can tell you where 53 port"),
     async execute(interaction) {
+        let who = "port is free!";
+
         try {
-            const port = await axios.get("https://mhp.inboost.ai:5053/api/who", {
+            // const port = await axios("https://mhp.inboost.ai:5053/api/who");
+            const response = await axios.get("https://mhp.inboost.ai:5053/api/who", {
                 headers: {
                     Authorization: `bearer ${inboostToken}`,
                 },
             });
 
-            await interaction.reply(port.data);
+            who = response.data;
         } catch (err) {
             console.log(err);
         }
+
+        await interaction.reply(who);
     },
 };
