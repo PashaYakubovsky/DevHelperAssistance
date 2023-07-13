b644e52ab266539f3e7e0a901673964b59fcc1cc
Pashafd <pasha.yakubovsky@lynksen.com>
2023-07-13 21:13:29 +0300

    +add translation +change endpoint

--- src/commands/where53.ts
+++ src/commands/where53.ts
@@ -2,6 +2,20 @@ import { SlashCommandBuilder } from "discord.js";
 import axios from "axios";
 import { bearerToken } from "../config.json";
 
+const translate = async (text: string): Promise<string> => {
+    try {
+        let res = await axios.get(
+            `https://api.mymemory.translated.net/get?q=${text}&langpair=ru|uk`
+        );
+        let translation = res.data.responseData.translatedText as string;
+        return translation;
+    } catch (err) {
+        console.error(err);
+    }
+
+    return text;
+};
+
 export const data = new SlashCommandBuilder()
     .setName("where53")
     .setDescription("I can tell you where 53 port");
@@ -11,13 +25,12 @@ export async function execute(interaction: any) {
     await interaction.deferReply();
 
     try {
-        // const port = await axios("https://mhp.inboost.ai:5053/api/who");
-        const response = await axios.get("http://localhost:5050/api/who", {
+        const response = await axios.get("https://mhp.inboost.ai:5053/api/Who", {
             headers: {
                 Authorization: `bearer ${bearerToken}`,
             },
         });
-        who = response.data;
+        who = await translate(response.data);
     } catch (err) {
         console.log(err);
     }
